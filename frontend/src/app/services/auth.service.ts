e2bd56dfdafead86870578d11193a41612d2884f
Phoenixovich <andreicristian004@gmail.com>
2025-01-06 23:27:06 +0200

    Reverted auth

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,27 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = '/api';
+  private apiUrl = 'http://localhost:5000/app'; 
   success: boolean = false;
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-    private cookieService: CookieService
-  ) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   login(username: string, password: string) {
+   
     return this.http.post(`${this.apiUrl}/login`, { username, password }, { withCredentials: true }).subscribe(
       (response: any) => {
-        this.cookieService.deleteAll();
         localStorage.setItem('jwt', response.accessToken);
+
         this.router.navigate(['/dashboard']);
+        
       },
       (error) => {
         console.error('Login failed', error);
@@ -30,39 +27,36 @@ export class AuthService {
   }
 
   logout() {
-    this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true }).subscribe(
-      () => {
-        localStorage.removeItem('jwt');
-        this.router.navigate(['/login']);
-      },
-      (error) => {
-        console.error('Logout failed', error);
-      }
-    );
+    this.http.get(`${this.apiUrl}/logout`).subscribe(() => {
+      
+      localStorage.removeItem('jwt');
+
+      this.router.navigate(['/login']);
+    });
   }
 
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('jwt');
+    const token = localStorage.getItem('jwt');
+
+
+    return !!token;
   }
 
   getToken() {
-    return localStorage.getItem('jwt');
+    const token = localStorage.getItem('jwt');
+    return token;
   }
-
-  register(username: string, password: string, firstname: string, lastname: string, email: string, company: string) {
-    return this.http.post(
-      `${this.apiUrl}/register`,
-      { username, password, firstname, lastname, email, company },
-      { withCredentials: true }
-    ).subscribe(
-      () => {
+  register(username: string, password: string,firstname:string,lastname:string,email:string,company:string) {
+    
+    return this.http.post(`${this.apiUrl}/register`, { username, password,firstname,lastname,email,company }, { withCredentials: true }).subscribe(
+      (response: any) => {
+       
         this.router.navigate(['/login']);
         this.success = true;
       },
       (error) => {
-        console.error('Registration failed', error);
+        console.error('Login failed', error);
       }
     );
   }
 }
-
