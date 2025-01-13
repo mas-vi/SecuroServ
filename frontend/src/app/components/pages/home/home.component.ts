832b9e1978570e5aebddfccdf077e0d4a7a22eac
Octavian <octavianmihaicretu@gmail.com>
2025-01-13 13:10:55 +0200

    Changed sign in button to dashboard when logged in

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { FooterComponent } from '../../partials/footer/footer.component';
 import { HeaderComponent } from '../../partials/header/header.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/auth.service';
 
 export interface DialogData {
   email: string;
@@ -10,10 +12,11 @@ export interface DialogData {
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, HeaderComponent, RouterModule],
+  imports: [FooterComponent, HeaderComponent, RouterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  authService = inject(AuthService);
 
 }
